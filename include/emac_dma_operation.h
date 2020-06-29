300051fcc72ee98c59c98b52a3efe9d85dd3726c
en2 <ernest.szczepaniak.en2@gmail.com>
2020-06-29 08:58:47 +0200

    dma operation

--- include/emac_dma_operation.h
+++ include/emac_dma_operation.h
@@ -14,16 +14,80 @@
 class Emac_dma_operation : public Emac_register
 {
 public:
+    enum class Receive_threshold : unsigned char
+    {
+        _64,
+        _32,
+        _96,
+        _128
+    }; /* enum: Threshold */
+
+    enum class Transmit_threshold : unsigned char
+    {
+        _64,
+        _128,
+        _192,
+        _256,
+        _40,
+        _32,
+        _24,
+        _16
+    }; /* enum: Transmit_threshold */    
+
+    enum class Flow_control : unsigned char 
+    {
+        _1KB,
+        _2KB,
+        _3KB,
+        _4KB
+    }; /* enum: Flow_control */
+
     Emac_dma_operation(unsigned int base);
 
     void receive(bool value);
     bool receive();
 
-    void process_second(bool value);
-    bool process_second(); 
+    void receive_parallel(bool value);
+    bool receive_parallel(); 
+
+    void receive_threshold(Receive_threshold value);
+    Receive_threshold receive_threshold();
+
+    void pass_undersized_frames(bool value);
+    bool pass_undersized_frames();
+
+    void pass_error_frames(bool value);
+    bool pass_error_frames();
+
+    void receive_flow_control(bool value);
+    bool receive_flow_control();
+
+    void receive_flow_control_activation(Flow_control value);
+    Flow_control receive_flow_control_activation();
+
+    void receive_flow_control_deactivation(Flow_control value);
+    Flow_control receive_flow_control_deactivation();
 
     void transmit(bool value);
     bool transmit();
+
+    void transmit_threshold(Transmit_threshold value);
+    Transmit_threshold transmit_threshold();
+
+    void transmit_flush(bool value);
+    bool transmit_flush();
+
+    void transmit_when_full(bool value);
+    bool transmit_when_full();
+
+    void receive_flush_disable(bool value);
+    bool receive_flush_disable();    
+
+    void receive_complete(bool value);
+    bool receive_complete();
+
+    void drop_crc_error_frames(bool value);
+    bool drop_crc_error_frames();
 
     void full_start(bool value);
     bool full_start();
--- source/emac_dma_operation.cpp
+++ source/emac_dma_operation.cpp
@@ -15,6 +15,76 @@ bool Emac_dma_operation::receive()
     return get(1, 1);
 }
 
+void Emac_dma_operation::receive_parallel(bool value)
+{
+    set(value, 2, 1);
+}
+
+bool Emac_dma_operation::receive_parallel()
+{
+    return get(2, 1);
+}
+
+void Emac_dma_operation::receive_threshold(Receive_threshold value)
+{
+    set(value, 3, 2);
+}
+
+Emac_dma_operation::Receive_threshold Emac_dma_operation::receive_threshold()
+{
+    return get<Receive_threshold>(3, 2);
+}
+
+void Emac_dma_operation::pass_undersized_frames(bool value)
+{
+    set(value, 6, 1);
+}
+
+bool Emac_dma_operation::pass_undersized_frames()
+{
+    return get(6, 1);
+}
+
+void Emac_dma_operation::pass_error_frames(bool value)
+{
+    set(value, 7, 1);
+}
+
+bool Emac_dma_operation::pass_error_frames()
+{
+    return get(7, 1);
+}
+
+void Emac_dma_operation::receive_flow_control(bool value)
+{
+    set(value, 8, 1);
+}
+
+bool Emac_dma_operation::receive_flow_control()
+{
+    return get(8, 1);
+}
+
+void Emac_dma_operation::receive_flow_control_activation(Flow_control value)
+{
+    set(value, 9, 2);
+}
+
+Emac_dma_operation::Flow_control Emac_dma_operation::receive_flow_control_activation()
+{
+    return get<Flow_control>(9, 2);
+}
+
+void Emac_dma_operation::receive_flow_control_deactivation(Flow_control value)
+{
+    set(value, 11, 2);
+}
+
+Emac_dma_operation::Flow_control Emac_dma_operation::receive_flow_control_deactivation()
+{
+    return get<Flow_control>(11, 2);
+}
+
 void Emac_dma_operation::transmit(bool value)
 {
     set(value, 13, 1);
@@ -25,12 +95,62 @@ bool Emac_dma_operation::transmit()
     return get(13, 1);
 }
 
-void Emac_dma_operation::full_start(bool value)
+void Emac_dma_operation::transmit_threshold(Transmit_threshold value)
+{
+    set(value, 14, 3);
+}
+
+Emac_dma_operation::Transmit_threshold Emac_dma_operation::transmit_threshold()
+{
+    return get<Transmit_threshold>(14, 3);
+}
+
+void Emac_dma_operation::transmit_flush(bool value)
+{
+    set(value, 20, 1);
+}
+
+bool Emac_dma_operation::transmit_flush()
+{
+    return get(20, 1);
+}
+
+void Emac_dma_operation::transmit_when_full(bool value)
 {
     set(value, 21, 1);
 }
 
-bool Emac_dma_operation::full_start()
+bool Emac_dma_operation::transmit_when_full()
 {
     return get(21, 1);
+}
+
+void Emac_dma_operation::receive_flush_disable(bool value)
+{
+    set(value, 24, 1);
+}
+
+bool Emac_dma_operation::receive_flush_disable()
+{
+    return get(24, 1);
+}
+
+void Emac_dma_operation::receive_complete(bool value)
+{
+    set(value, 25, 1);
+}
+
+bool Emac_dma_operation::receive_complete()
+{
+    return get(25, 1);
+}
+
+void Emac_dma_operation::drop_crc_error_frames(bool value)
+{
+    set(value, 26, 1);
+}
+
+bool Emac_dma_operation::drop_crc_error_frames()
+{
+    return get(26, 1);
 }
